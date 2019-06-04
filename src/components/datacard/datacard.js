63bf7cda0d965ab8be4305453024b4e8fd4572a2
rahulsai1999 <noneuser2183@gmail.com>
2019-06-04 13:25:26 +0530

    datacard configured for search also

--- src/components/datacard/datacard.js
+++ src/components/datacard/datacard.js
@@ -1,29 +1,60 @@
-import React, { Component } from 'react';
-import './datacard.css'
+import React, { Component } from "react";
+import "./datacard.css";
 
 class DataCard extends Component {
-    constructor() {
-        super();
-        this.state = {}
-    }
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-    render() {
-        const { data } = this.props;
-        return (
-            <div className="datacard">
-                <div className="row">
-                    <div className="col">
-                        <img className="datacard-img" alt="poster" src={data.images.poster}></img>
-                    </div>
-                    <div className="col">
-                        <div className="row"><h5>{data.title}</h5></div>
-                        <div className="row"><h6>{data.year}</h6></div>
-                    </div>
-                </div>
+  render() {
+    const { data } = this.props;
+    if (data["Type"]) {
+      return (
+        <div className="datacard">
+          <div className="row">
+            <div className="col">
+              <img
+                className="datacard-img"
+                alt="poster"
+                src={data["Poster"]}
+              />
             </div>
-        )
+            <div className="col">
+              <div className="row">
+                <h5>{data["Title"]}</h5>
+              </div>
+              <div className="row">
+                <h6>{data["Year"]}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="datacard">
+          <div className="row">
+            <div className="col">
+              <img
+                className="datacard-img"
+                alt="poster"
+                src={data.images.poster}
+              />
+            </div>
+            <div className="col">
+              <div className="row">
+                <h5>{data.title}</h5>
+              </div>
+              <div className="row">
+                <h6>{data.year}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     }
+  }
 }
 
 export default DataCard;
-
