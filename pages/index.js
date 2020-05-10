0d084177c039118e7f2a3342c70179b22474c701
rahulsai1999 <noneuser2183@gmail.com>
2020-05-10 21:28:54 +0530

    built autocomplete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { AutoComplete, Input } from "antd";
+import { useState } from "react";
+import Axios from "axios";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const onSearchFunc = (value) => {
+    setSearch(value);
+    search.length > 3
+      ? Axios.get("http://api.tvmaze.com/search/shows", {
+          params: { q: search },
+        }).then((response) => {
+          const { data } = response;
+          setResults(data);
+        })
+      : null;
+  };
+
   return (
     <div className="container">
       <Head>
@@ -10,51 +28,32 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://tvfind2.github.com">TVFind</a>
         </h1>
+        <AutoComplete
+          placeholder="Search from thousands of TV Shows"
+          onSearch={onSearchFunc}
+          size="large"
+          style={{ width: "80%", marginTop: "10%" }}
+        >
+          {search.length > 3 && results.length != 0
+            ? results.map((item) => {
+                const { name, externals } = item.show;
+                return (
+                  <AutoComplete.Option key={name} value={externals.imdb}>
+                    {name}
+                  </AutoComplete.Option>
+                );
+              })
+            : null}
+        </AutoComplete>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div className="grid"></div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
+        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -105,12 +104,6 @@ export default function Home() {
         .title a {
           color: #0070f3;
           text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
         }
 
         .title {
@@ -205,5 +198,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
